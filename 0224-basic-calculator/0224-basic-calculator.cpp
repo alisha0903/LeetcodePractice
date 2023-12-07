778952f62fde480ba63803d92092ddbdec354632
Alisha Raghav <92165013+alisha0903@users.noreply.github.com>
2023-12-07 15:12:06 +0530

    Time: 7 ms (66.88%), Space: 8.5 MB (54.63%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n = s.size();
+        int sign = 1;
+        int sum = 0;
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                int num = 0;
+                while (i < n && s[i] >= '0' && s[i] <= '9') {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                sum += num * sign;
+                i--;
+            } else if (s[i] == '+') {
+                sign = 1;
+            } else if (s[i] == '-') {
+                sign = -1;
+            } else if (s[i] == '(') {
+                st.push(sum);
+                st.push(sign);
+                sum = 0;
+                sign = 1;
+            } else if (s[i] == ')') {
+                int prevSign = st.top();
+                st.pop();
+                int prevSum = st.top();
+                st.pop();
+                sum = prevSum + (prevSign * sum);
+            }
+        }
+
+        return sum;
+    }
+};
